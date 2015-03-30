040e33218a366ab4fbef360006cfd5566a9614be
saurabh-deochake <saurabh.d04@gmail.com>
2015-03-30 14:36:28 +0530

    added classes

--- tweennow.py
+++ tweennow.py
@@ -23,50 +23,46 @@ Access Token and Access Secret Keys from https://dev.twitter.com by creating you
 own app
 
 """
-
-def authenticate():
-    print CONFIG_FILE
     
-    parser = SafeConfigParser()
-    parser.read(CONFIG_FILE)
-    
-    ckey = parser.get('token','ckey')
-    csecret = parser.get('token','csecret')
-    atoken = parser.get('token','atoken')
-    asecret = parser.get('token','asecret')
-    
-    return ckey,csecret,atoken, asecret
 
-if version_info.major is 2:
-    import pynotify
-    notifyModule = "pynotify"
-if version_info.major is 3:
-    try:
-        from gi.Repository import Notify
-        notifyModule = "Notify"
-    except ImportError:
-        import notify2
-        notifyModule = "notify2"
 
-#using pynotify module to send messages on desktop        
-def popUpMessage(message):
-    if notifyModule is "pynotify":
-        logging.debug("Initializing pynotify")
-        pynotify.init("Tweennow")
-        logging.debug("Sending notification: message:{}".format(message))
-        pynotify.Notification(message).show()
-    elif notifyModule is "Notify":
-        logging.debug("Initializing Notify")
-        Notify.init("Tweennow")
-        logging.debug("Sending notification: message:{}".format(message))
-        Notify.Notification.new(message).show()
-    else:
-        logging.debug("Initializing notify2")
-        notify2.init("Tweennow")
-        logging.debug("Sending notification: message:{}".format(message))
-        notify2.Notification(message).show()
+class Tweennow:
+    
         
-class listener(StreamListener):
+    def authenticate(self):
+        print CONFIG_FILE
+        
+        parser = SafeConfigParser()
+        parser.read(CONFIG_FILE)
+        
+        ckey = parser.get('token','ckey')
+        csecret = parser.get('token','csecret')
+        atoken = parser.get('token','atoken')
+        asecret = parser.get('token','asecret')
+        
+        return ckey,csecret,atoken, asecret
+    
+
+class Messenger(StreamListener):
+    
+    #using pynotify module to send messages on desktop        
+    def popUpMessage(self,message):
+        if notifyModule is "pynotify":
+            logging.debug("Initializing pynotify")
+            pynotify.init("Tweennow")
+            logging.debug("Sending notification: message:{}".format(message))
+            pynotify.Notification(message).show()
+        elif notifyModule is "Notify":
+            logging.debug("Initializing Notify")
+            Notify.init("Tweennow")
+            logging.debug("Sending notification: message:{}".format(message))
+            Notify.Notification.new(message).show()
+        else:
+            logging.debug("Initializing notify2")
+            notify2.init("Tweennow")
+            logging.debug("Sending notification: message:{}".format(message))
+            notify2.Notification(message).show()
+        
 
     def on_data(self, data):
         try:
@@ -80,7 +76,8 @@ class listener(StreamListener):
             fetchedTweet = "@"+userName+"-"+tweet
             
             #Call popUpMessage() to send your message to desktop
-            popUpMessage(fetchedTweet)
+            
+            self.popUpMessage(fetchedTweet)
             time.sleep(7)
             return True
         
@@ -96,11 +93,27 @@ class listener(StreamListener):
     def on_error(self, status):
         print status
 
-ckey, csecret, atoken, asecret = authenticate()
-auth = OAuthHandler(ckey, csecret)
+if __name__ == '__main__':
+    
+    if version_info.major is 2:
+        import pynotify
+        notifyModule = "pynotify"
+    if version_info.major is 3:
+        try:
+            from gi.Repository import Notify
+            notifyModule = "Notify"
+        except ImportError:
+            import notify2
+            notifyModule = "notify2"
 
-print "Auth done"
-auth.set_access_token(atoken, asecret)
-print "access done"
-twitterStream = Stream(auth, listener())
-twitterStream.filter(track=["#AUSvNZ"])  #Track tweets with any hashtags/Word
+    
+    tObj = Tweennow()
+    
+    ckey, csecret, atoken, asecret = tObj.authenticate()
+    auth = OAuthHandler(ckey, csecret)
+    
+    print "Auth done"
+    auth.set_access_token(atoken, asecret)
+    print "access done"
+    twitterStream = Stream(auth, Messenger())
+    twitterStream.filter(track=["AAP"])  #Track tweets with any hashtags/Word
